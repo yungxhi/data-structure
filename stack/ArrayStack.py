e0d6d814bf49c2a7382aabaed48bb29e235dbf21
이윤지 <yoonji0879@dgsw.hs.kr>
2025-09-16 12:21:57 +0900

    프로그램 추가

--- stack/ArrayStack.py
+++ stack/ArrayStack.py
@@ -34,3 +34,19 @@ class ArrayStack :
       return self.array[self.top]
     else:
       print("stack underflow")
+
+  def __str__(self) :
+    return str(self.array[0:self.top+1])
+
+
+if __name__ == "__main__" :
+  s = ArrayStack(10)
+  for i in range(1, 6):
+    s.push(i)
+  # print("push 5회", s)
+  print("push 5회", s.array)
+  s.pop()
+  print('pop 1회', s)
+  s.pop()
+  print('pop 2회', s)
+  print(s.peek())
